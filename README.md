69e5095d4e47ac0d1ea060ab59971662b75e0891
Antoine Brun <antoine.brun@gmail.com>
2025-04-17 12:25:57 +0200

    add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Disable the automatic update to prevent unintended changes to workflows during local development.
+
+mkdir -p /opt/fmc_repository/OpenMSA_Workflow_Kibana/.devops 
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+. /usr/share/install-libraries/il-lib.sh
+
+pushd /opt/fmc_repository/Process || exit;
+emit_step "Linking Tutorial."
+mk_meta_link "OpenMSA_Workflow_Tutorials" "Tutorials"
+popd || exit
+
