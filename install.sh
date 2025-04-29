cbd978e5e61f97e7a0aefec6dc87e8a7500ec908
Antoine Brun <antoine.brun@gmail.com>
2025-04-29 17:25:28 +0200

    install Cisco MS for WF tutorial

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 . /usr/share/install-libraries/il-lib.sh
 
 pushd /opt/fmc_repository/Process || exit;
-emit_step "Linking Tutorial."
+emit_step "Linking Workflow Tutorial."
 mk_meta_link "OpenMSA_Workflow_Tutorials" "Tutorials"
 popd || exit
 
+pushd /opt/fmc_repository/CommandDefinition || exit;
+emit_step "Linking Microservice Tutorial."
+mk_meta_link "OpenMSA_Workflow_Tutorials" "Tutorials"
+popd || exit
