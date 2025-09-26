77e5da32d88d59f3a4fb4a4a10c818fe6f62bf0f
ydu <ydu@ubiqube.com>
2025-09-26 17:22:21 +0200

    Revamp multilevel workflows

--- Tutorials/multi_level_execution/MultiLayerLevel0/Process_Init/Tasks/Task_execute.py
+++ Tutorials/multi_level_execution/MultiLayerLevel0/Process_Init/Tasks/Task_execute.py
@@ -6,6 +6,8 @@ from msa_sdk import util
 import json
 
 dev_var = Variables()
+dev_var.add('nb_l1', var_type='Integer')
+dev_var.add('nb_l2', var_type='Integer')
 context = Variables.task_call(dev_var)
 
 ubiqube_id = context['UBIQUBEID']
@@ -13,32 +15,30 @@ async_update_list = (context['PROCESSINSTANCEID'], context['TASKID'], context['E
 
 SERVICE_NAME = 'Process/Tutorials/multi_level_execution/MultiLayerLevel1/MultiLayerLevel1'
 CREATE_PROCESS_NAME = 'Process/Tutorials/multi_level_execution/MultiLayerLevel1/Process_L1_Execute_L2'
-processes = []
 
-for i in range(100):
-    orch = Orchestration(ubiqube_id)
+orch = Orchestration(ubiqube_id)
+processes = {}
+nb = int(context['nb_l1'])
+
+for i in range(nb):
     data = dict()
     data["input1"] = f"Process L1 instance {i+1}"
+    data['nb_l2'] = context['nb_l2']
     orch.execute_service(SERVICE_NAME, CREATE_PROCESS_NAME, data)
     response = json.loads(orch.content)
-    service_id = response.get('serviceId').get('id')
     process_id = response.get('processId').get('id')
-   
-    processes.append({"process_id": process_id, "status": "RUNNING"})
-    orch.update_asynchronous_task_details(*async_update_list, str(processes))   
+    processes[process_id] = "RUNNING"
+    orch.update_asynchronous_task_details(*async_update_list, f'{i} L1 running process')   
 
-still_running = True
-while still_running:
-    still_running = False
-    for p in processes:
-        if p["status"] == "RUNNING":
-            orch = Orchestration(ubiqube_id)
-            status = orch.get_process_status_by_id(p["process_id"])
-            p["status"] = status
-            if status not in ["ENDED", "FAIL"]:  # process not completed
-                still_running = True  # at least one still running
-    orch.update_asynchronous_task_details(*async_update_list, str(processes))
-    time.sleep(5)  # wait before next poll
+total_process = len(processes)
+still_running = total_process
+while processes:
+  for process_id in list(processes):
+    status = orch.get_process_status_by_id(process_id)
+    if status != "RUNNING":  # process completed
+        del processes[process_id]
+  still_running = len(processes)
+  orch.update_asynchronous_task_details(*async_update_list, f'{still_running} / {total_process} L1 running process')
+  time.sleep(5)  # wait before next poll
 
 MSA_API.task_success('L0 Execution Task OK', context, True)
-
